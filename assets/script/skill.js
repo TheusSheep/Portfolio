4bfdfb2db350e786e67a9bd430e1705af2e7a367
TSheep <mggodmine@gmail.com>
2024-10-08 11:45:55 -0300

    Portfolio v1.1
    Consegui fazer a interação dos botões funcionar só que ele não funciona caso a tela fique maior , criando um outro bug de glitch para resolver. Pelo menos é um bug visual.

--- assets/script/skill.js
+++ assets/script/skill.js
@@ -1,25 +1,52 @@
-let html = document.querySelector('#item_html>i')
-html.addEventListener('mouseover', mouseHTML)
-html.addEventListener('mouseout', saiHTML)
+const item_html = document.querySelector('#html>i')
+const item_css = document.querySelector('#css>i')
+const item_git = document.querySelector('#git>i')
+const item_github = document.querySelector('#github>i')
+const item_js = document.querySelector('#js>i')
+
+let text_html = document.querySelector('#html>p')
+let text_css = document.querySelector('#css>p')
+let text_git = document.querySelector('#git>p')
+let text_github = document.querySelector('#github>p')
+let text_js = document.querySelector('#js>p')
 
 
+item_html.addEventListener('mouseover', function(){
+    text_html.style.display = 'block'
+})
 
-function mouseHTML(){
-    let text = document.querySelector('#item_html_text')
-    
-    if (text.style.display == 'block'){
-         text.style.display = 'none'
-     } else {
-        text.style.display = 'block'
-     }
-}
+item_html.addEventListener('mouseout', function(){
+    text_html.style.display = 'none'
+})
 
-function saiHTML(){
-    let text = document.querySelector('#item_html_text')
+item_css.addEventListener('mouseover', function(){
+    text_css.style.display = 'block'
+})
 
-    if (text.style.display == 'none'){
-        text.style.diplay = 'block'
-    } else {
-        text.style.display = 'none'
-    }
-}
+item_css.addEventListener('mouseout', function(){
+    text_css.style.display = 'none'
+})
+
+item_git.addEventListener('mouseover', function(){
+    text_git.style.display = 'block'
+})
+
+item_git.addEventListener('mouseout', function(){
+    text_git.style.display = 'none'
+})
+
+item_github.addEventListener('mouseover', function(){
+    text_github.style.display = 'block'
+})
+
+item_github.addEventListener('mouseout', function(){
+    text_github.style.display = 'none'
+})
+
+item_js.addEventListener('mouseover', function(){
+    text_js.style.display = 'block'
+})
+
+item_js.addEventListener('mouseout', function(){
+    text_js.style.display = 'none'
+})
